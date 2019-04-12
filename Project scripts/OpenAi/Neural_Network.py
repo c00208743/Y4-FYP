008cae7b03c42222e99063a3ed3394bcb8dce0a6
Jamie Murphy <c00208743@itcarlow.ie>
2019-04-12 20:22:32 +0100

    Update Neural_Network.py

--- Project scripts/OpenAi/Neural_Network.py
+++ Project scripts/OpenAi/Neural_Network.py
@@ -9,14 +9,14 @@ from collections import Counter
 
 import time
 
-LR = 1e-3
+LR = 1e-3 # learning rate
 env = gym.make('CartPole-v0')
 env.reset()
-goal_steps = 500
-score_requirement = 50
-initial_games = 10000
+goal_steps = 500 # limit on frames and game choices
+score_requirement = 50 # score limit for training data
+initial_games = 10000 # amount of games to train
 
-def some_random_games_first():
+def some_random_games_first(): # play game with random movements
     for episode in range(5):
         env.reset()
         for t in range(goal_steps):
@@ -29,19 +29,19 @@ def some_random_games_first():
 ##some_random_games_first()
 
 def initial_population():
-    training_data = []
-    scores = []
-    accepted_scores = []
+    training_data = [] # training data sample
+    scores = [] # all scores
+    accepted_scores = [] # scores of successful random choice games
     for _ in range(initial_games):
         score = 0
         game_memory = []
         prev_obseration = []
         for _ in range(goal_steps):
-            action = random.randrange(0, 2)
-            observation, reward, done, info = env.step(action)
+            action = random.randrange(0, 2) # decide action
+            observation, reward, done, info = env.step(action) # move forward a frame with action
 
             if len(prev_obseration) > 0:
-                game_memory.append([prev_obseration, action])
+                game_memory.append([prev_obseration, action]) # remember action choosen in previous state
 
             prev_obseration = observation
             score +=reward
@@ -57,7 +57,7 @@ def initial_population():
                 elif data[1] == 0:
                     output = [1,0]
 
-                training_data.append([data[0], output])
+                training_data.append([data[0], output]) # add to traing data
                 #print(training_data)
 
         env.reset()
@@ -74,13 +74,13 @@ def initial_population():
     return training_data
 
 def neural_network_model(input_size):
-    network = input_data(shape = [None, input_size, 1], name='input')
+    network = input_data(shape = [None, input_size, 1], name='input') #input layer
 
-    network = fully_connected(network, 3, activation='relu')
+    network = fully_connected(network, 3, activation='relu') # hidden layer
     network = dropout(network, 0.8)
 
 
-    network = fully_connected(network, 2, activation='softmax')
+    network = fully_connected(network, 2, activation='softmax') # output layer
     network = regression(network, optimizer='adam', learning_rate=LR,
                         loss='categorical_crossentropy', name='targets')
     model = tflearn.DNN(network, tensorboard_dir='log')
@@ -93,7 +93,7 @@ def train_model(training_data, model=False):
     Y = [i[1] for i in training_data]
 
     if not model:
-        model = neural_network_model(input_size = len(X[0]))
+        model = neural_network_model(input_size = len(X[0])) # train model with training sample
 
     model.fit(X, Y, n_epoch=3, snapshot_step=500, show_metric=True,
                 run_id='openaistuff')
@@ -110,20 +110,20 @@ for each_game in range(10):
     score = 0
     game_memory = []
     prev_obs = []
-    env.reset()
+    env.reset() # reset environment
     for _ in range(goal_steps):
         env.render()
-        time.sleep(0.1)
+        time.sleep(0.1) # render slower --> demo purposes
         if len(prev_obs) == 0:
-            action = random.randrange(0, 2)
+            action = random.randrange(0, 2) # choose random action
             #action = env.action_space.sample()
         else:
-            action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs), 1))[0])
+            action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs), 1))[0]) # use nn to predict an action
         choices.append(action)
 
         new_observation, reward, done, info = env.step(action)
         prev_obs = new_observation
-        game_memory.append([new_observation, action])
+        game_memory.append([new_observation, action]) # record how action influenced the env
         score += reward
         if done:
             break
